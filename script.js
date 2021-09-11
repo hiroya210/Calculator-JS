6a9c23aa672a74a941b766aa31c438ce4e0367d4
Hiroya <hirotakemura210@gmail.com>
2021-09-12 06:26:16 +0900

    added new js Scripts

--- script.js
+++ script.js
@@ -1,31 +1,71 @@
-const calcdisplay = document.querySelector("input");
+const calcDisplay = document.querySelector("input");
+const calcButton = document.querySelector("#btn");
+let arr = [];
+let total = 0;
+let operation = "";
+console.log(calcDisplay.value)
 
-function add(num1, num2){
-    return num1 + num2;
-};
 
-function subtract(num1, num2){
-    return num1 - num2;
-};
-
-function multiply(num1, num2){
-    return num1*num2;
-};
-
-function divide(num1,num2){
-    return num1/num2;
-};
-
-function operate(ope,num1,num2){
-    if (ope = "+"){
-        add(num1,num2);
+function numButton(e){
+    calcDisplay.value = calcDisplay.value + e
+    if (calcDisplay.value.length === 20){
+     let arr = calcDisplay.value.split("");
+       arr.pop();
+       calcDisplay.value = arr.join("");
+       calcDisplay.value;
     }
+ }
+
+
+
+function opPress(ope){
+        operation = ope;
+        num1 = calcDisplay.value;
+        arr[0] = num1;
+        calcDisplay.value = "";
+        console.log(arr)
+    }
+
+    
+    
+    
+function operate(){
+    num2 = calcDisplay.value;
+    arr[1] = (num2)
+    let arr2 = arr.map(function toInt(z){
+        return parseFloat(z);
+    })
+    console.log(arr2)
+    if(operation == "+"){
+        total = arr2.reduce(function total(a,b){
+                return a + b
+            },0);
+    }
+    else if(operation == "-"){
+        total = arr2.reduce(function total(a,b){
+                return a - b
+        })
+    }
+    else if(operation == "*"){
+        total = arr2.reduce(function total(a,b){
+                return a * b
+        },1)
+    }
+    else if(operation == "/"){
+        total = arr2.reduce(function total(a,b){
+                return a / b
+        })
+    }
+    
+    
+    arr2[1] = total;
+    console.log(total)
+    calcDisplay.value = total;
+    
 }
 
-function input(e){
-    calcdisplay.value = calcdisplay.value + e.value
+function clearDisplay(){
+    arr = []
+    calcDisplay.value = ""
 }
 
-function clear(val){
-     clearBtn.value = val
-}
